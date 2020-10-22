f3d13a6de951440669aba9b26941490fd4137072
Drew Brooks <w0726128@myscc.ca>
2020-10-22 01:28:44 -0400

    Completed Asn2

--- angular-work/src/app/content-card/content-card.component.ts
+++ angular-work/src/app/content-card/content-card.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import {ContentList} from '../helper-files/content-list';
+import {Component, Input, OnInit} from '@angular/core';
 import {Content} from '../helper-files/content-interface';
 
 @Component({
@@ -9,42 +8,12 @@ import {Content} from '../helper-files/content-interface';
 })
 
 export class ContentCardComponent implements OnInit {
-
-  contentList: ContentList;
-
+  @Input()content: Content;
   constructor() {
-    this.contentList = new ContentList([]);
-
-    const content1: Content = {
-      id: 0,
-      title: 'World of Warcraft',
-      body: 'An MMORPG',
-      author: 'Blizzard',
-      imgUrl: 'https://www.extremetech.com/wp-content/uploads/2018/04/BattleforFeature.jpg',
-      tags: ['MMORPG', 'Video-Game']
-    };
-    const content2: Content = {
-      id: 1,
-      title: 'Overwatch',
-      body: 'Another Blizzard game.',
-      author: 'Blizzard',
-      imgUrl: 'https://overwatch2-static.playoverwatch.com/db9fad9b1afa62ece79b992af07bc84414f432dc/static/images/share.jpg',
-      tags: ['Video-Game', 'Colourful']
-    };
-    const content3: Content = {
-      id: 2,
-      title: 'Paper Mario: Thousand Year Door',
-      body: 'A Nintendo game.',
-      author: 'Nintendo',
-      imgUrl: 'https://images.nintendolife.com/6df1a314e5ce1/paper-mario-the-thousand-year-door.original.jpg',
-      tags: ['Video-Game', 'Puzzle']
-    };
-
-    this.contentList.increaseArray(content1);
-    this.contentList.increaseArray(content2);
-    this.contentList.increaseArray(content3);
   }
   ngOnInit(): void {
   }
-
+  displayID(){
+    console.log(this.content.id);
+  }
 }
--- angular-work/src/app/content-list/content-list.component.ts
+++ angular-work/src/app/content-list/content-list.component.ts
@@ -1,13 +1,60 @@
 import { Component, OnInit } from '@angular/core';
+import {Content} from '../helper-files/content-interface';
 
 @Component({
   selector: 'app-content-list',
   templateUrl: './content-list.component.html',
   styleUrls: ['./content-list.component.scss']
 })
+
+
 export class ContentListComponent implements OnInit {
 
-  constructor() { }
+  contentList: Content[];
+  constructor() {
+    this.contentList = [
+     {
+      id: 0,
+      title: 'World of Warcraft',
+      body: 'An MMORPG',
+      author: 'Blizzard',
+      imgUrl: 'https://www.extremetech.com/wp-content/uploads/2018/04/BattleforFeature.jpg',
+      tags: ['MMORPG', 'Video-Game']
+    },
+    {
+      id: 1,
+      title: 'Overwatch',
+      body: 'Another Blizzard game.',
+      author: 'Blizzard',
+      imgUrl: 'https://overwatch2-static.playoverwatch.com/db9fad9b1afa62ece79b992af07bc84414f432dc/static/images/share.jpg',
+      tags: ['Video-Game', 'Colourful']
+    },
+    {
+      id: 2,
+      title: 'Paper Mario: Thousand Year Door',
+      body: 'A Nintendo game.',
+      author: 'Nintendo',
+      imgUrl: 'https://images.nintendolife.com/6df1a314e5ce1/paper-mario-the-thousand-year-door.original.jpg',
+      tags: ['Video-Game', 'Puzzle']
+    },
+      {
+        id: 2,
+        title: 'Paper Mario: Thousand Year Door',
+        body: 'A Nintendo game.',
+        author: 'Nintendo',
+        imgUrl: 'https://images.nintendolife.com/6df1a314e5ce1/paper-mario-the-thousand-year-door.original.jpg',
+        tags: ['Video-Game', 'Puzzle']
+      },
+      {
+        id: 2,
+        title: 'Paper Mario: Thousand Year Door',
+        body: 'A Nintendo game.',
+        author: 'Nintendo',
+        imgUrl: 'https://images.nintendolife.com/6df1a314e5ce1/paper-mario-the-thousand-year-door.original.jpg',
+        tags: ['Video-Game', 'Puzzle']
+      }
+    ];
+  }
 
   ngOnInit(): void {
   }
